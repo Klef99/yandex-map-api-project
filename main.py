c93342922a6fe0bdfa98abb90c57d85130c26ff5
Egor <20607279+Klef99@users.noreply.github.com>
2022-01-27 17:40:38 +0900

    Add base files

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)  # Загружаем дизайн
+        #.pushButton.clicked.connect(self.run)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def run(self):
+        self.label.setText("OK")
+        # Имя элемента совпадает с objectName в QTDesigner
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
